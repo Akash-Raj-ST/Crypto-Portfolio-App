a1b02186a88f7b992a3bb7ae593c088536717fb0
Akash Raj ST <akashraj49070@gmail.com>
2022-01-07 10:57:09 +0530

    documentUpload //done

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,16 +1,22 @@
 import React from 'react'
-import { View} from 'react-native'
+import { ScrollView, View} from 'react-native'
 import { Divider } from 'react-native-elements/dist/divider/Divider'
 
 import BottomTabs from '../Components/BottomTabs'
 import Balance from '../Components/Profile/Balance'
+import Document from '../Components/Profile/Document'
 import Info from '../Components/Profile/Info'
 
 export default function Profile() {
     return (
         <View style={{flex:1,justifyContent:'space-between'}}>
             <Info/>
-            <Balance/>
+            <Divider/>
+            <ScrollView>           
+                <Document/>
+                <Divider/>
+                <Balance/>
+            </ScrollView>
             <BottomTabs currentTab="Profile"/>
         </View>
     )
